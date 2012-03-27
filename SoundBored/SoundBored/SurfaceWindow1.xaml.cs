fa3834b7f7b9acdd81f863eba9f7c7ed7f889526
mikkujacob <mikkujacob@f3aba3c3-db5e-d35a-3ac1-2c3edf386a38>
2012-03-27 03:56:49 +0000

    Added Chromatic Scale Layout Fix
    Added Pattern & PatternUnit DataStructures
    
    Added Multitouch MultiNote Playing fux

--- SoundBored/SoundBored/SurfaceWindow1.xaml.cs
+++ SoundBored/SoundBored/SurfaceWindow1.xaml.cs
@@ -17,6 +17,7 @@ using Microsoft.Surface.Presentation;
 using Microsoft.Surface.Presentation.Controls;
 using Microsoft.Surface.Presentation.Input;
 using Bespoke.Common.Osc;
+using System.Collections;
 
 namespace SoundBored
 {
@@ -30,9 +31,13 @@ namespace SoundBored
         private static int dstPort = 6655;
         private static IPEndPoint src = new IPEndPoint(IPAddress.Loopback, srcPort);
         private static IPEndPoint dst = new IPEndPoint(IPAddress.Loopback, dstPort);
+        private static int EIdx;
         private static int CuedButtonNo;
         private static int NoOfKeys = 26;
         private Rectangle[] Buttons = new Rectangle[26];
+
+        //ArrayList Pattern has elements of type PatternUnit converted to Object, so don't forget data conversions when adding or removing elements from Pattern
+        private ArrayList Pattern = new ArrayList();
 
         /// <summary>
         /// Default constructor.
@@ -113,8 +118,42 @@ namespace SoundBored
             //TODO: disable audio, animations here
         }
 
+        //Called as soon as Canvas C is loaded... Init screen...
+        private void C_Loaded(object sender, RoutedEventArgs e)
+        {
+            Buttons[0] = B0;
+            Buttons[1] = B1;
+            Buttons[2] = B2;
+            Buttons[3] = B3;
+            Buttons[4] = B4;
+            Buttons[5] = B5;
+            Buttons[6] = B6;
+            Buttons[7] = B7;
+            Buttons[8] = B8;
+            Buttons[9] = B9;
+            Buttons[10] = B10;
+            Buttons[11] = B11;
+            Buttons[12] = B12;
+            Buttons[13] = B13;
+            Buttons[14] = B14;
+            Buttons[15] = B15;
+            Buttons[16] = B16;
+            Buttons[17] = B17;
+            Buttons[18] = B18;
+            Buttons[19] = B19;
+            Buttons[20] = B20;
+            Buttons[21] = B21;
+            Buttons[22] = B22;
+            Buttons[23] = B23;
+            Buttons[24] = B24;
+            Buttons[25] = B25;
+
+            TransformToThirteenKeys();
+            showVisualCue(getRandomButton());
+        }
+
         //Changes layout to 6 keys 0 - 5
-        private void TransformToTwelveKeys()
+        private void TransformToThirteenKeys()
         {
             int LeftAmount = 65;
             int TopAmount = 500;
@@ -129,37 +168,6 @@ namespace SoundBored
 
                 Buttons[i].Margin = new Thickness(LeftAmount, TopAmount, 0, 0);
                 Buttons[i].Width = 110;
-                Buttons[i].Height = 360;
-                Buttons[i].Visibility = System.Windows.Visibility.Visible;
-
-                LeftAmount += LeftIncrement;
-            }
-        }
-
-        //Changes layout to 6 keys 0 - 5
-        private void TransformToSixKeys()
-        {
-            int LeftAmount = 80;
-            int TopAmount = 500;
-            int LeftIncrement = 316;
-            for (int i = 0; i < NoOfKeys; i++)
-            {
-                if ((i > 5 && i < 13) || (i > 20 && i < 26))
-                { 
-                    Buttons[i].Margin = new Thickness(10000, 10000, 0, 0);
-                    Buttons[i].Width = 180;
-                    Buttons[i].Height = 360;
-                    Buttons[i].Visibility = System.Windows.Visibility.Collapsed;
-                    continue;
-                }
-                else if (i == 13)
-                {
-                    LeftAmount = 80;
-                    TopAmount = 60;
-                }
-
-                Buttons[i].Margin = new Thickness(LeftAmount, TopAmount, 0, 0);
-                Buttons[i].Width = 180;
                 Buttons[i].Height = 360;
                 Buttons[i].Visibility = System.Windows.Visibility.Visible;
 
@@ -198,6 +206,37 @@ namespace SoundBored
             }
         }
 
+        //Changes layout to 6 keys 0 - 5 & 13 - 18
+        private void TransformToSixKeys()
+        {
+            int LeftAmount = 80;
+            int TopAmount = 500;
+            int LeftIncrement = 316;
+            for (int i = 0; i < NoOfKeys; i++)
+            {
+                if ((i > 5 && i < 13) || (i > 18 && i < 26))
+                {
+                    Buttons[i].Margin = new Thickness(10000, 10000, 0, 0);
+                    Buttons[i].Width = 180;
+                    Buttons[i].Height = 360;
+                    Buttons[i].Visibility = System.Windows.Visibility.Collapsed;
+                    continue;
+                }
+                else if (i == 13)
+                {
+                    LeftAmount = 80;
+                    TopAmount = 60;
+                }
+
+                Buttons[i].Margin = new Thickness(LeftAmount, TopAmount, 0, 0);
+                Buttons[i].Width = 180;
+                Buttons[i].Height = 360;
+                Buttons[i].Visibility = System.Windows.Visibility.Visible;
+
+                LeftAmount += LeftIncrement;
+            }
+        }
+
         //Add an ellipse to Rectangle R
         private void showVisualCue(Rectangle R)
         {
@@ -206,6 +245,7 @@ namespace SoundBored
 
             int idx = Int32.Parse(R.Name.Substring(1));
             CuedButtonNo = idx;
+            EIdx = idx;
 
             E.Visibility = System.Windows.Visibility.Visible;
             E.Margin = new Thickness(R.Margin.Left, R.Margin.Top, 0, 0);
@@ -214,42 +254,9 @@ namespace SoundBored
         //Hide Ellipse from screen
         private void hideVisualCue()
         {
+            EIdx = -1;
             E.Margin = new Thickness(1000, 1000, 0, 0);
             E.Visibility = System.Windows.Visibility.Collapsed;
-        }
-
-        //Called as soon as Canvas C is loaded... Init screen...
-        private void C_Loaded(object sender, RoutedEventArgs e)
-        {
-            Buttons[0] = B0;
-            Buttons[1] = B1;
-            Buttons[2] = B2;
-            Buttons[3] = B3;
-            Buttons[4] = B4;
-            Buttons[5] = B5;
-            Buttons[6] = B6;
-            Buttons[7] = B7;
-            Buttons[8] = B8;
-            Buttons[9] = B9;
-            Buttons[10] = B10;
-            Buttons[11] = B11;
-            Buttons[12] = B12;
-            Buttons[13] = B13;
-            Buttons[14] = B14;
-            Buttons[15] = B15;
-            Buttons[16] = B16;
-            Buttons[17] = B17;
-            Buttons[18] = B18;
-            Buttons[19] = B19;
-            Buttons[20] = B20;
-            Buttons[21] = B21;
-            Buttons[22] = B22;
-            Buttons[23] = B23;
-            Buttons[24] = B24;
-            Buttons[25] = B25;
-
-            TransformToTwelveKeys();
-            showVisualCue(getRandomButton());
         }
 
         //Send an OSC Message
@@ -258,6 +265,11 @@ namespace SoundBored
         {
             bool isCued = false;
             OscMessage m;
+
+            if (CuedButtonNo != EIdx)
+            {
+                return false;
+            }
 
             if (CuedButtonNo == idx)
             {
@@ -324,7 +336,7 @@ namespace SoundBored
         {
             //TODO: Add colour change or Ellipse pressed effect...
 
-            bool handled = sendNoteOsc(CuedButtonNo);
+            bool handled = sendNoteOsc(EIdx);
 
             FrameworkElement fe = e.Source as FrameworkElement;
             Console.WriteLine("__\n [TouchDown] " + fe.Name);
@@ -340,13 +352,50 @@ namespace SoundBored
         {
             int Random = (new Random()).Next(26);
 
-            //JUST A TEMPORARY TESTING LINE OF CODE... CAN'T SEE THESE FOUR KEYS ON MY SCREEN. ;)
-            //while (Random == 6 || Random == 7 || Random == 14 || Random == 15)//TESTING
+            ////JUST A TEMPORARY TESTING LINE OF CODE... CAN'T SEE THESE FOUR KEYS ON MY SCREEN. ;)
+            //while (Random > 9 && Random < 13 || Random > 22 && Random < 26)//TESTING
             //{//TESTING
-            //    Random = (new Random()).Next(16);//TESTING
+            //    Random = (new Random()).Next(26);//TESTING
             //}//TESTING
 
             return Buttons[Random];
+        }
+    }
+
+    class PatternUnit
+    {
+        private int Note;
+        private int Duration;
+
+        public PatternUnit()
+        { 
+            
+        }
+
+        public PatternUnit(int Note, int Duration)
+        {
+            this.Note = Note;
+            this.Duration = Duration;
+        }
+
+        public int GetNote()
+        {
+            return this.Note;
+        }
+
+        public void SetNote(int Note)
+        {
+            this.Note = Note;
+        }
+
+        public int GetDuration()
+        {
+            return this.Duration;
+        }
+
+        public void SetDuration(int Duration)
+        {
+            this.Duration = Duration;
         }
     }
 }
